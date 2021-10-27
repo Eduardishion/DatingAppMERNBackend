dd5ecb958bfecca06671540b21fefda863b4ab14
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-10-27 16:51:43 -0500

    instalando cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const appRoutes = require('./routes/appRoutes');
+const Cors = require('cors');
 
 //configuracion de app
 const app = express();
@@ -10,6 +11,9 @@ const port = process.env.PORT || 8001;
 const connection_url = 'mongodb+srv://root:12345@appmerndb.ragum.mongodb.net/appmernDB?retryWrites=true&w=majority'
 
 //Middlewares
+
+app.use(express.json())
+app.use(Cors())
 
 //configuracion de base de datos con mongoose
 //https://exerror.com/mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported/
